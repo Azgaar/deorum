7414c6ac7e675ec52871d0ade1c8a5d384a63c34
max <maxganiev@yandex.com>
2022-09-21 02:49:30 +0300

    feat: gallery - selection and drawer

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import './global.scss';
+</script>
+
+<main>
+  <slot />
+</main>
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import ImageList, { Item, ImageAspectContainer, Image } from '@smui/image-list';
+  import IconButton from '@smui/icon-button';
   import Checkbox from '@smui/checkbox';
 
   import { URL } from '$lib/config';
@@ -15,7 +16,10 @@
   };
 
   const { portraits, originals } = data;
+  const portraitsMap = new Map(portraits.map((p) => [p.id, p]));
+
   let selected: string[] = [];
+  $: lastSelected = selected.length && portraitsMap.get(selected.at(-1)!);
 
   const collectionId = portraits[0]?.['@collectionId'];
   const imagesPath = `${URL}/api/files/${collectionId}`;
@@ -23,23 +27,59 @@
   const handleClick = (id: string) => () => {
     selected = selected.includes(id) ? selected.filter((item) => item !== id) : [...selected, id];
   };
+
+  const handleClearSelection = () => {
+    selected = [];
+  };
 </script>
 
-<ImageList class="my-image-list-standard">
-  {#each portraits as item (item.id)}
-    <Item on:click={handleClick(item.id)}>
-      <ImageAspectContainer>
-        <div class="imageContainer">
+<main>
+  <ImageList class="gallery">
+    {#each portraits as item (item.id)}
+      <Item on:click={handleClick(item.id)}>
+        <ImageAspectContainer>
+          <div class="imageContainer">
+            <Image
+              loading="lazy"
+              alt={originals.get(item.original)}
+              src={`${imagesPath}/${item.id}/${item.image}`}
+            />
+            <div class="checkbox" class:hidden={!selected.length && !selected.includes(item.id)}>
+              <Checkbox checked={selected.includes(item.id)} touch ripple={false} />
+            </div>
+          </div>
+        </ImageAspectContainer>
+      </Item>
+    {/each}
+  </ImageList>
+
+  <div class="drawer" class:open={lastSelected}>
+    <div class="drawerContent">
+      {#if lastSelected}
+        <ImageAspectContainer>
           <Image
             loading="lazy"
-            alt={originals.get(item.original)}
-            src={`${imagesPath}/${item.id}/${item.image}`}
+            alt={selected.at(0)}
+            src={`${imagesPath}/${lastSelected.id}/${lastSelected.image}`}
           />
-          <div class="checkbox" class:hidden={!selected.length && !selected.includes(item.id)}>
-            <Checkbox checked={selected.includes(item.id)} touch ripple={false} />
+
+          <div class="imageOverlay" class:hidden={selected.length < 2}>
+            <span>{selected.length}</span>
           </div>
-        </div>
-      </ImageAspectContainer>
-    </Item>
-  {/each}
-</ImageList>
+
+          <IconButton
+            on:click={handleClearSelection}
+            class="material-icons closeButton"
+            title="Remove">close</IconButton
+          >
+        </ImageAspectContainer>
+      {/if}
+
+      <ul>
+        {#each selected as item}
+          <li>{item}</li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</main>
