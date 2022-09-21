2d85cccd45a6c18a5c12cc3f4fa52a0dcccd9940
max <maxganiev@yandex.com>
2022-09-22 02:17:56 +0300

    feat: editor - footer buttons

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -15,6 +15,30 @@
   $: currentColors = model.colors;
   $: currentTags = model.tags.map((tag) => [tag, tags.get(tag)!]);
   $: currentStyles = model.styles.map((style) => [style, styles.get(style)!]);
+
+  let isChanged = false;
+
+  const handleRemoveColor = (color: string) => () => {
+    currentColors = currentColors.filter((item) => item !== color);
+    isChanged = true;
+  };
+
+  const handleRemoveTag = (tagId: string) => () => {
+    currentTags = currentTags.filter(([id]) => id !== tagId);
+    isChanged = true;
+  };
+
+  const handleRemoveStyle = (styleId: string) => () => {
+    currentStyles = currentStyles.filter(([id]) => id !== styleId);
+    isChanged = true;
+  };
+
+  const handleCancel = () => {
+    currentColors = model.colors;
+    currentTags = model.tags.map((tag) => [tag, tags.get(tag)!]);
+    currentStyles = model.styles.map((style) => [style, styles.get(style)!]);
+    isChanged = false;
+  };
 </script>
 
 <section class="editor">
@@ -32,7 +56,7 @@
       <span class="chip">
         <span class="color" style="color:{color}">⬤</span>
         {color}
-        <span class="action">✕</span>
+        <span on:click={handleRemoveColor(color)} class="action">✕</span>
       </span>
     {/each}
     <span class="chip action">✏️</span>
@@ -41,7 +65,10 @@
   <div>
     Tags:
     {#each currentTags as [tagId, tagName] (tagId)}
-      <span class="chip">{tagName} <span class="action">✕</span></span>
+      <span class="chip">
+        {tagName}
+        <span on:click={handleRemoveTag(tagId)} class="action">✕</span>
+      </span>
     {/each}
     <span class="chip action">✏️</span>
   </div>
@@ -49,16 +76,24 @@
   <div>
     Styles:
     {#each currentStyles as [styleId, styleName] (styleId)}
-      <span class="chip">{styleName} <span class="action">✕</span></span>
+      <span class="chip">
+        {styleName}
+        <span class="action" on:click={handleRemoveStyle(styleId)}>✕</span>
+      </span>
     {/each}
     <span class="chip action">✏️</span>
   </div>
 </section>
 
+<footer>
+  <button disabled={!isChanged} class="button">Save</button>
+  <button disabled={!isChanged} on:click={handleCancel} class="button">Cancel</button>
+</footer>
+
 <style>
   section.editor {
     padding: 0.5em;
-    font-size: 20px;
+    font-size: 22px;
   }
 
   div {
@@ -103,5 +138,24 @@
   span.color {
     font-size: smaller;
     vertical-align: text-bottom;
+  }
+
+  footer {
+    display: flex;
+    justify-self: end;
+    flex-grow: 1;
+    align-items: flex-end;
+    justify-content: space-between;
+  }
+
+  footer > button {
+    border: none;
+    font-size: 32px;
+    padding: 0.5em;
+    width: 50%;
+  }
+
+  footer > button:hover {
+    background: #999;
   }
 </style>
