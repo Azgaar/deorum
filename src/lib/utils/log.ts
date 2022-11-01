039cd7a730ce933152fd01324f5ac03c21d2a3f4
max <maxganiev@yandex.com>
2022-11-01 23:37:38 +0300

    feat: format logs

--- src/lib/utils/log.ts
+++ src/lib/utils/log.ts
@@ -1,16 +1,19 @@
 import { get } from 'svelte/store';
 
 import { user } from '$lib/stores';
+import { dev } from '$app/environment';
 
 const style = 'color: #fff; background: #000; padding: 2px 4px; border-radius: 3px;';
 
 export const log = (domain: string, message: string, ...args: unknown[]) => {
-  console.info(`%c${domain}%c ${message}`, style, '', { user: get(user) }, ...args);
+  if (dev) console.info(`%c${domain}`, style, message);
+  else console.info(`${domain}: ${message}`, { user: get(user) }, ...args);
 };
 
 export const report = (domain: string, error: unknown, ...args: unknown[]) => {
   const message = error instanceof Error ? error.message : error;
   const stack = error instanceof Error ? error.stack : '';
 
-  console.error(`%c${domain}%c ${message}`, style, '', { user: get(user) }, stack, ...args);
+  if (dev) console.error(`%c${domain}`, style, message);
+  else console.error(`${domain}: ${message}`, { user: get(user) }, stack, ...args);
 };
