bc3af871b03a4a492a637afb8a6c2485efa1dd70
max <maxganiev@yandex.com>
2023-01-24 17:17:05 +0400

    refactor: split server handlers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,14 @@
-import { ADMIN_USERNAME, ADMIN_PASSWORD } from '$env/static/private';
+import { sequence } from '@sveltejs/kit/hooks';
 import type { Cookies } from '@sveltejs/kit';
 
+import { ADMIN_USERNAME, ADMIN_PASSWORD } from '$env/static/private';
 import admin from '$lib/api/admin';
 import { authorize, isSignedIn } from '$lib/api/auth';
 import { COOKIE_NAME } from '$lib/config';
 import { Role } from '$lib/stores';
 import { report } from '$lib/utils/log';
 
-const redirect = (sourceUrl: string) =>
+const redirectToSignIn = (sourceUrl: string) =>
   new Response(null, {
     status: 307,
     headers: { location: `/signin?unauthorized=true&to=${sourceUrl}` }
@@ -33,25 +34,35 @@ const getPageLanguage = (cookies: Cookies) => {
   }
 };
 
-export const handle: import('@sveltejs/kit').Handle = async ({ event, resolve }) => {
-  const target = new URL(event.request.url).pathname;
+const protectRoutes: import('@sveltejs/kit').Handle = async ({ event, resolve }) => {
+  const url = event.request.url;
+  const target = new URL(url).pathname;
 
   const isProtected = protectedRoutes.some((route) => target.includes(route));
   if (isProtected) {
     const cookie = event.request.headers.get('cookie');
-    if (!cookie) return redirect(target);
+    if (!cookie) return redirectToSignIn(target);
 
     const user = await authorize(cookie);
-    if (!user) return redirect(target);
+    if (!user) return redirectToSignIn(target);
 
     const allowedRoutes = routesProtection[user?.profile?.role];
-    if (!allowedRoutes.some((route) => event.request.url.includes(route))) return redirect(target);
+    if (allowedRoutes.every((route) => !url.includes(route))) return redirectToSignIn(target);
   }
 
-  // login as executer: evergreen admin user to run PocketBase requests on guest user behalf
-  if (!isSignedIn(admin)) await admin.users.authViaEmail(ADMIN_USERNAME, ADMIN_PASSWORD);
+  return resolve(event);
+};
 
+// login as evergreen admin user to run PocketBase requests on guest user behalf
+const loginAsExecuter: import('@sveltejs/kit').Handle = async ({ event, resolve }) => {
+  if (!isSignedIn(admin)) await admin.users.authViaEmail(ADMIN_USERNAME, ADMIN_PASSWORD);
+  return resolve(event);
+};
+
+const setPageLanguage: import('@sveltejs/kit').Handle = async ({ event, resolve }) => {
   return resolve(event, {
     transformPageChunk: ({ html }) => html.replace('%lang%', getPageLanguage(event.cookies))
   });
 };
+
+export const handle = sequence(protectRoutes, loginAsExecuter, setPageLanguage);
