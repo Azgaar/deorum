b5168d64fc47a6bbe997579a1aa0cbd74d8f1377
max <maxganiev@yandex.com>
2023-03-04 02:01:41 +0400

    feat(char editor): cache character data

--- src/routes/api/characters/[slug]/+server.ts
+++ src/routes/api/characters/[slug]/+server.ts
@@ -3,16 +3,16 @@ import admin from '$lib/api/admin';
 import { invalidateCache } from '$lib/cache/cacheInstance';
 import { createServerError } from '$lib/utils/errors';
 import { log, report } from '$lib/utils/log';
+import { getElement } from '$lib/api/getData';
 
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params, url }) => {
   try {
     const id = params.slug;
-    const filter = url.searchParams.get('filter') || '';
     const expand = url.searchParams.get('expand') || '';
 
-    const character = await admin.records.getOne('characters', id, { filter, expand });
+    const character = await getElement('characters', id, expand);
     log('characters', `Loading character ${id}`);
     return json(character);
   } catch (err) {
