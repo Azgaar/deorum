d4c11dabae8988ee0e26638afdf77512e7ac3bf2
max <maxganiev@yandex.com>
2023-05-28 01:10:46 +0400

    feat: disable web vitals

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,10 @@
   import Dialog from '$lib/components/dialog/Dialog.svelte';
   import Snackbar from '$lib/components/snackbar/Snackbar.svelte';
   import LoadingOverlay from '$lib/components/spinner/LoadingOverlay.svelte';
-  import { webVitals } from '$lib/scripts/vitals';
   import { inject } from '@vercel/analytics';
 
   if (!dev) {
     inject({ mode: 'production' });
-    webVitals({ page: $page.route.id || '' });
   }
 </script>
 
