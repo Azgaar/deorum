353d90db011caa82124b9085d463e45dfbba4fa5
Azgaar <azgaar.fmg@yandex.com>
2024-04-13 16:55:11 +0200

    chore: pump version to 1.1

--- svelte.config.js
+++ svelte.config.js
@@ -6,11 +6,9 @@ const config = {
   preprocess: sveltePreprocess({
     scss: { prependData: '@use "src/theme/variables.scss" as *;' }
   }),
-
   kit: {
     adapter: adapter()
   },
-
   files: {
     lib: 'src/lib'
   }
